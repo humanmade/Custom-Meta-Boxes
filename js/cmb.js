1b7c1f1f41d34d864a7247c4cb691a00de86f7fd
Jonathan Bardo <bardo.jonathan@gmail.com>
2013-04-22 12:20:11 -0400

    Add description to group. + Add possibility to sort group with handle

--- js/cmb.js
+++ js/cmb.js
@@ -5,39 +5,15 @@
 /*jslint browser: true, devel: true, indent: 4, maxerr: 50, sub: true */
 /*global jQuery, tb_show, tb_remove */
 
+/**
+ * Callback handler.
+ *
+ * Use the methods addCallbackForInit, addCallbackForClonedField and addCallbackForDeleteField
+ * Use these to add custom code for your fields.
+ */
+
 var CMB = {
 	
-	_callbacks: [],
-	
-	addCallbackForClonedField: function( fieldName, callback ) {
-
-		if ( jQuery.isArray( fieldName ) )
-			for ( var i = 0; i < fieldName.length; i++ )
-				CMB.addCallbackForClonedField( fieldName[i], callback );
-
-		this._callbacks[fieldName] = this._callbacks[fieldName] ? this._callbacks[fieldName] : []
-		this._callbacks[fieldName].push( callback )
-	},
-	
-	clonedField: function( el ) {
-
-		var _this = this
-		
-		// also check child elements
-		el.add( el.find( 'div[data-class]' ) ).each( function(i, el) {
-
-			el = jQuery( el )
-			var callbacks = _this._callbacks[el.attr( 'data-class') ]
-		
-			if ( callbacks ) {
-				for (var a = 0; a < callbacks.length; a++) {
-					callbacks[a]( el )
-				}
-			}
-
-		})
-	},
-
 	_initCallbacks: [],
 
 	addCallbackForInit: function( callback ) {
@@ -48,19 +24,78 @@ var CMB = {
 
 	init: function() {
 
-		var _this = this;
+		var _this = this,
+			callbacks = _this._initCallbacks;
+		
+		if ( callbacks ) {
+			for ( var a = 0; a < callbacks.length; a++) {
+				callbacks[a]();
+			}
+		}
+
+	},
+	
+	_clonedFieldCallbacks: [],
+	
+	addCallbackForClonedField: function( fieldName, callback ) {
+		
+		if ( jQuery.isArray( fieldName ) )
+			for ( var i = 0; i < fieldName.length; i++ )
+				CMB.addCallbackForClonedField( fieldName[i], callback );
+
+		this._clonedFieldCallbacks[fieldName] = this._clonedFieldCallbacks[fieldName] ? this._clonedFieldCallbacks[fieldName] : []
+		this._clonedFieldCallbacks[fieldName].push( callback )
+	
+	},
+	
+	clonedField: function( el ) {
+
+		var _this = this
 		
 		// also check child elements
+		el.add( el.find( 'div[data-class]' ) ).each( function(i, el) {
+
+			el = jQuery( el )
+			var callbacks = _this._clonedFieldCallbacks[el.attr( 'data-class') ]
 		
-		var callbacks = _this._initCallbacks;
+			if ( callbacks )
+				for ( var a = 0; a < callbacks.length; a++ )
+					callbacks[a]( el );
+
+		})
+	},
+
+	_deletedFieldCallbacks: [],
+
+	addCallbackForDeletedField: function( fieldName, callback ) {
+
+		if ( jQuery.isArray( fieldName ) )
+			for ( var i = 0; i < fieldName.length; i++ )
+				CMB.addCallbackForClonedField( fieldName[i], callback );
+	
+		this._deletedFieldCallbacks[fieldName] = this._deletedFieldCallbacks[fieldName] ? this._deletedFieldCallbacks[fieldName] : []
+		this._deletedFieldCallbacks[fieldName].push( callback )
+	
+	},
+
+	deletedField: function( el ) {
+
+		var _this = this
 		
-		if ( callbacks )
-			for ( var a = 0; a < callbacks.length; a++)
-				callbacks[a]();
-			
+		// also check child elements
+		el.add( el.find( 'div[data-class]' ) ).each( function(i, el) {
+		
+			el = jQuery( el )
+			var callbacks = _this._deletedFieldCallbacks[el.attr( 'data-class') ]
+		
+			if ( callbacks )
+				for ( var a = 0; a < callbacks.length; a++ )
+					callbacks[a]( el )
+				
+		})
 	}
 
-};
+}
 
 jQuery(document).ready(function ($) {
 
@@ -71,28 +106,31 @@ jQuery(document).ready(function ($) {
 
 	CMB.init();
 
-	$('.CMB_Group_Field').sortable({
+	jQuery('.CMB_Group_Field').sortable({
 		cancel: '.mceStatusbar', 
 		handle: '.move-field',
 		items: "> .field-item",
 		start: function(event, ui) { // turn TinyMCE off while sorting (if not, it won't work when resorted)
-			// $('.cmb_wysiwyg').each(function(){
-			// 	tinyMCE.execCommand('mceRemoveControl', false, $(this).attr('id') );
-			// })
+			$('.wp-editor-area').each(function(){
+				tinyMCE.execCommand('mceRemoveControl', false, $(this).attr('id') );
+			})
 		},
 		stop: function(event, ui) { // re-initialize TinyMCE when sort is completed
-			// $('.cmb_wysiwyg:not(:hidden)').each(function(){
-			// 	tinyMCE.execCommand('mceAddControl', false, $(this).attr('id') );
-			// })
+			$('.wp-editor-area:not(:hidden)').each(function(){
+				tinyMCE.execCommand('mceAddControl', false, $(this).attr('id') );
+			})
 		}
 	});
 
 	jQuery( document ).on( 'click', '.delete-field', function( e ) {
 
 		e.preventDefault();
-		var a = jQuery( this );
 
-		a.closest( '.field-item' ).remove();
+		var fieldItem = jQuery(this).closest( '.field-item' );
+
+		CMB.deletedField( fieldItem );	
+
+		fieldItem.remove();
 
 	} );
 
@@ -104,7 +142,6 @@ jQuery(document).ready(function ($) {
 	    var newT = el.prev().clone();
 
 	    newT.removeClass('hidden');
-	    
 	    newT.find('input[type!="button"]').not('[readonly]').val('');
 	    newT.find( '.cmb_upload_status' ).html('');
 	    newT.insertBefore( el.prev() );
@@ -112,14 +149,14 @@ jQuery(document).ready(function ($) {
 	    // Recalculate group ids & update the name fields..
 		var index = 0;
 		var field = $(this).closest('.field' );
-		var attrs = ['id','name','for'];	
+		var attrs = ['id','name','for','data-id','data-name'];	
 		
 		field.children('.field-item').not('.hidden').each( function() {
 
-			var search  = field.hasClass( 'CMB_Group_Field' ) ? /cmb-group-(\d|x)*/ : /cmb-field-(\d|x)*/;
+			var search  = field.hasClass( 'CMB_Group_Field' ) ? /cmb-group-(\d|x)*/g : /cmb-field-(\d|x)*/g;
 			var replace = field.hasClass( 'CMB_Group_Field' ) ? 'cmb-group-' + index : 'cmb-field-' + index;
 
-			$(this).find('[id],[for],[name]').each( function() {
+			$(this).find( '[' + attrs.join('],[') + ']' ).each( function() {
 
 				for ( var i = 0; i < attrs.length; i++ )
 					if ( typeof( $(this).attr( attrs[i] ) ) !== 'undefined' )
@@ -133,74 +170,6 @@ jQuery(document).ready(function ($) {
 
 	    CMB.clonedField( newT )
 
-	} );
-
-});
-
-
-/**
- * ColorPickers
- */
-
-CMB.addCallbackForInit( function() {
-
-	// Colorpicker
-	jQuery('input:text.cmb_colorpicker').wpColorPicker();
-
-} );
-
-CMB.addCallbackForClonedField( 'CMB_Color_Picker', function( newT ) {
-
-	// Reinitialize colorpickers
-    newT.find('.wp-color-result').remove();
-	newT.find('input:text.cmb_colorpicker').wpColorPicker();
-
-} );
-
-
-/**
- * Date & Time Fields
- */
-
-CMB.addCallbackForClonedField( ['CMB_Date_Field', 'CMB_Time_Field', 'CMB_Date_Timestamp_Field', 'CMB_Datetime_Timestamp_Field' ], function( newT ) {
-
-	// Reinitialize all the datepickers
-	newT.find( '.cmb_datepicker' ).each(function () {
-		jQuery(this).attr( 'id', '' ).removeClass( 'hasDatepicker' ).removeData( 'datepicker' ).unbind().datepicker();
-	});
-
-	// Reinitialize all the timepickers.
-	newT.find('.cmb_timepicker' ).each(function () {
-		jQuery(this).timePicker({
-			startTime: "07:00",
-			endTime: "22:00",
-			show24Hours: false,
-			separator: ':',
-			step: 30
-		});
-	});
-
-} );
-
-CMB.addCallbackForInit( function() {
-
-	// Datepicker
-	jQuery('.cmb_datepicker').each(function () {
-		jQuery(this).datepicker();
-	});
-	
-	// Wrap date picker in class to narrow the scope of jQuery UI CSS and prevent conflicts
-	jQuery("#ui-datepicker-div").wrap('<div class="cmb_element" />');
-
-	// Timepicker
-	jQuery('.cmb_timepicker').each(function () {
-		jQuery(this).timePicker({
-			startTime: "07:00",
-			endTime: "22:00",
-			show24Hours: false,
-			separator: ':',
-			step: 30
-		});
 	} );
 
 });
