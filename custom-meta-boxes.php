36c48d1de32db5428a24570cd1aa84fdcf9036fe
Matth_eu <matthew@matth.eu>
2014-03-31 23:38:59 +0100

    big licence from codex

--- custom-meta-boxes.php
+++ custom-meta-boxes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Custom Meta Boxes
 Plugin URI: https://github.com/humanmade/Custom-Meta-Boxes
@@ -8,6 +7,19 @@ Version: 1.0.1
 License: GPL-2.0+
 Author: Human Made Limited
 Author URI: http://hmn.md
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 if ( ! defined( 'CMB_DEV') )
