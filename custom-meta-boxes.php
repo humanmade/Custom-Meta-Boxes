2d0a4ac4a566b311644894a499706deb80269365
Matth_eu <matthew@matth.eu>
2014-04-01 10:40:45 +0100

    fix description to mention that this is a fork

--- custom-meta-boxes.php
+++ custom-meta-boxes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Custom Meta Boxes
 Plugin URI: https://github.com/humanmade/Custom-Meta-Boxes
-Description: Lets you easily create metaboxes with custom fields that will blow your mind.
+Description: Lets you easily create metaboxes with custom fields that will blow your mind. Originally a fork of https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress.
 Version: 1.0.1
 License: GPL-2.0+
 Author: Human Made Limited
