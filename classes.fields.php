c7512b9b32237093c23e290b77d8be60e54ca33c
Matth_eu <matthew@matth.eu>
2014-08-16 01:14:42 +0100

    Refactor + add elevation field

--- classes.fields.php
+++ classes.fields.php
@@ -1459,6 +1459,8 @@ class CMB_Group_Field extends CMB_Field {
  *
  * It enables the google places API and doesn't store the place
  * name. It only stores latitude and longitude of the selected area.
+ *
+ * Note
  */
 class CMB_Gmap_Field extends CMB_Field {
 
@@ -1467,20 +1469,39 @@ class CMB_Gmap_Field extends CMB_Field {
 		parent::enqueue_scripts();
 
 		wp_enqueue_script( 'cmb-google-maps', '//maps.google.com/maps/api/js?sensor=true&libraries=places' );
-		wp_enqueue_script( 'cmb-google-maps-script', trailingslashit( CMB_URL ) . 'js/cmb-gmap.js', array( 'jquery' ) );
+		wp_enqueue_script( 'cmb-google-maps-script', trailingslashit( CMB_URL ) . 'js/field-gmap.js', array( 'jquery', 'cmb-google-maps' ) );
+
+		wp_localize_script( 'cmb-google-maps-script', 'CMBGmaps', array(
+			'defaults' => array(
+				'latitude'  => '51.5073509',
+				'longitude' => '-0.12775829999998223',
+				'zoom'      => '4',
+			),
+			'strings'  => array(
+				'markerTitle' => __( 'Drag to set the exact location', 'cmb' )
+			)
+		) );
+
 	}
 
 	public function html() {
+
 		$value = $this->get_value();
-		$lat   = isset( $value['lat'] ) ? $value['lat'] : '54.800685';
-		$long  = isset( $value['long'] ) ? $value['long'] : '-4.130859';
+
+		$value = wp_parse_args(
+			$this->get_value(),
+			array( 'lat' => null, 'long' => null, 'elevation' => null )
+		);
+
 		?>
 
 		<input type="text" <?php $this->class_attr( 'map-search' ); ?> <?php $this->id_attr(); ?> />
-		<div class="map" style="width: 100%; height: 250px; border: 1px solid #eee; margin-top: 10px;"></div>
 
-		<input type="hidden" class="latitude" <?php $this->name_attr( '[lat]' ); ?> value="<?php echo $lat; ?>" />
-		<input type="hidden" class="longitude" <?php $this->name_attr( '[long]' ); ?> value="<?php echo $long; ?>" />
+		<div class="map" style="width: 100%; height: 250px; border: 1px solid #eee; margin-top: 8px;"></div>
+
+		<input type="hidden" class="latitude"  <?php $this->name_attr( '[lat]' ); ?>       value="<?php echo esc_attr( $value['lat'] ); ?>" />
+		<input type="hidden" class="longitude" <?php $this->name_attr( '[long]' ); ?>      value="<?php echo esc_attr( $value['long'] ); ?>" />
+		<input type="hidden" class="elevation" <?php $this->name_attr( '[elevation]' ); ?> value="<?php echo esc_attr( $value['elevation'] ); ?>" />
 
 		<?php
 	}
--- js/field-gmap.js
+++ js/field-gmap.js
@@ -3,80 +3,90 @@
 
 (function($) {
 
-    var CMBGmapsInit = function( fieldEl ) {
+	var CMBGmapsInit = function( fieldEl ) {
 
-        var searchInput = $('.map-search', fieldEl ).get(0);
-        var mapCanvas   = $('.map', fieldEl ).get(0);
-        var latitude    = $('.latitude', fieldEl );
-        var longitude   = $('.longitude', fieldEl );
+		var searchInput = $('.map-search', fieldEl ).get(0);
+		var mapCanvas   = $('.map', fieldEl ).get(0);
+		var latitude    = $('.latitude', fieldEl );
+		var longitude   = $('.longitude', fieldEl );
+		var elevation   = $('.elevation', fieldEl );
+		var elevator    = new google.maps.ElevationService();
 
-        console.log( fieldEl );
-        console.log( latitude );
-        console.log( longitude );
+		var mapOptions = {
+			center:    new google.maps.LatLng( CMBGmaps.defaults.latitude, CMBGmaps.defaults.longitude ),
+			zoom:      parseInt( CMBGmaps.defaults.zoom ),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
 
-        var mapOptions = {
-            center:    new google.maps.LatLng( CMBGmaps.defaults.latitude, CMBGmaps.defaults.longitude ),
-            zoom:      parseInt( CMBGmaps.defaults.zoom ),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+		var map      = new google.maps.Map( mapCanvas, mapOptions );
 
-        var map = new google.maps.Map( mapCanvas, mapOptions );
+		// Marker
+		var markerOptions = {
+			map: map,
+			draggable: true,
+			title: CMBGmaps.strings.markerTitle
+		};
 
-        // Marker
-        var markerOptions = {
-            map: map,
-            draggable: true,
-            title: CMBGmaps.strings.markerTitle
-        };
+		var marker = new google.maps.Marker( markerOptions );
 
-        var marker = new google.maps.Marker( markerOptions );
+		function setPosition( latLng, zoom ) {
 
-        // Set stored Coordinates
-        if ( latitude.val() && longitude.val() ) {
-            latLng = new google.maps.LatLng( latitude.val(), longitude.val() );
-            marker.setPosition(latLng);
-            map.setCenter( latLng );
-            map.setZoom(17);
-        }
+			marker.setPosition( latLng );
+			map.setCenter( latLng );
+			map.setZoom( zoom );
 
-        google.maps.event.addListener(marker, 'drag', function() {
-            latitude.val(marker.getPosition().lat());
-            longitude.val(marker.getPosition().lng());
-        });
+			latitude.val( latLng.lat() );
+			longitude.val( latLng.lng() );
 
-        // Search
-        var autocomplete = new google.maps.places.Autocomplete(searchInput);
-        autocomplete.bindTo('bounds', map);
+			elevator.getElevationForLocations( { locations: [ marker.getPosition() ] }, function (results, status) {
+				if (status == google.maps.ElevationStatus.OK && results[0] ) {
+					elevation.val( results[0].elevation );
+				}
+			});
 
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            var place = autocomplete.getPlace();
-            if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-            } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);
-            }
+		}
 
-            marker.setPosition(place.geometry.location);
+		// Set stored Coordinates
+		if ( latitude.val() && longitude.val() ) {
+			latLng = new google.maps.LatLng( latitude.val(), longitude.val() );
+			setPosition( latLng, 17 )
+		}
 
-            latitude.val(place.geometry.location.lat());
-            longitude.val(place.geometry.location.lng());
-        });
+		google.maps.event.addListener( marker, 'dragend', function() {
+			setPosition( marker.getPosition(), 17 );
+		});
 
-        $(searchInput).keypress(function(e) {
-            if (e.keyCode === 13) {
-                e.preventDefault();
-            }
-        });
+		// Search
+		var autocomplete = new google.maps.places.Autocomplete(searchInput);
+		autocomplete.bindTo('bounds', map);
 
-    }
+		google.maps.event.addListener(autocomplete, 'place_changed', function() {
+			var place = autocomplete.getPlace();
+			if (place.geometry.viewport) {
+				map.fitBounds(place.geometry.viewport);
+			} else {
+				map.setCenter(place.geometry.location);
+				map.setZoom(17);
+			}
 
-    CMB.addCallbackForInit( function() {
-        $('.CMB_Gmap_Field .field-item').each(function() {
-            CMBGmapsInit( $(this) );
-        });
-    } );
+			setPosition( place.geometry.location, 17 );
 
-    CMB.addCallbackForClonedField( ['CMB_Gmap_Field'], CMBGmapsInit );
+		});
+
+		$(searchInput).keypress(function(e) {
+			if (e.keyCode === 13) {
+				e.preventDefault();
+			}
+		});
+
+	}
+
+	CMB.addCallbackForInit( function() {
+		$('.CMB_Gmap_Field .field-item').each(function() {
+			CMBGmapsInit( $(this) );
+		});
+	} );
+
+	CMB.addCallbackForClonedField( ['CMB_Gmap_Field'], CMBGmapsInit );
 
 }(jQuery));
