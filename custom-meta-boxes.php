1318f902b747c4deeea745bfb5bc32141afc27a8
Matth_eu <matthew@matth.eu>
2014-09-07 23:06:59 +0100

    Bump version

--- custom-meta-boxes.php
+++ custom-meta-boxes.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Meta Boxes
 Plugin URI: https://github.com/humanmade/Custom-Meta-Boxes
 Description: Lets you easily create metaboxes with custom fields that will blow your mind. Originally a fork of https://github.com/jaredatch/Custom-Metaboxes-and-Fields-for-WordPress.
-Version: 1.0.1
+Version: 1.0.2
 License: GPL-2.0+
 Author: Human Made Limited
 Author URI: http://hmn.md
