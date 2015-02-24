4b972391012b11909e1aa4a4a69ed81fe550f90c
Matth_eu <matthew@matth.eu>
2015-02-24 15:24:20 +0000

    Fix JS

--- js/field.datetime.js
+++ js/field.datetime.js
@@ -4,10 +4,20 @@
 	 * Date & Time Fields
 	 */
 
-	function CMBDateFieldInit() {
+	function CMBDateFieldInit( $container ) {
+
+		var $dateFields, $timeFields;
+
+		if ( $container ) {
+			$dateFields = $container.find( '.cmb_datepicker' );
+			$timeFields = $container.find( '.cmb_timepicker' );
+		} else {
+			$dateFields = $( '.cmb_datepicker' )
+			$timeFields = $( '.cmb_timepicker' )
+		}
 
 		// Reinitialize all the datepickers
-		$( '.cmb_datepicker' ).each(function () {
+		$dateFields.each(function () {
 
 			var val, field;
 
@@ -26,10 +36,10 @@
 		});
 
 		// Wrap date picker in class to narrow the scope of jQuery UI CSS and prevent conflicts
-		jQuery("#ui-datepicker-div").wrap('<div class="cmb_element" />');
+		$("#ui-datepicker-div").wrap('<div class="cmb_element"/>');
 
 		// Timepicker
-		jQuery('.cmb_timepicker').each(function () {
+		$timeFields.each(function () {
 			jQuery(this).timePicker({
 				startTime: "00:00",
 				endTime: "23:30",
@@ -41,8 +51,8 @@
 
 	}
 
-	CMB.addCallbackForClonedField( ['CMB_Date_Field', 'CMB_Time_Field', 'CMB_Date_Timestamp_Field', 'CMB_Datetime_Timestamp_Field' ], function( newT ) {
-		CMBDateFieldInit();
+	CMB.addCallbackForClonedField( ['CMB_Date_Field', 'CMB_Time_Field' ], function( $container ) {
+		CMBDateFieldInit( $container );
 	} );
 
 	CMB.addCallbackForInit( function() {
