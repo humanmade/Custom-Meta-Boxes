924dcc332b2d5eb340eb76a543469b81e8a707ba
mikeselander <mikeselander@gmail.com>
2016-11-13 13:00:35 +0100

    Swapped out files for new Select2 4.0.3 files and added version extension

--- classes.fields.php
+++ classes.fields.php
@@ -1063,7 +1063,7 @@ class CMB_Select extends CMB_Field {
 
 		parent::enqueue_scripts();
 
-		wp_enqueue_script( 'select2', trailingslashit( CMB_URL ) . 'js/vendor/select2/select2.js', array( 'jquery' ) );
+		wp_enqueue_script( 'select2', trailingslashit( CMB_URL ) . 'js/vendor/select2/select2.min.js', array( 'jquery' ), '4.0.3' );
 		wp_enqueue_script( 'field-select', trailingslashit( CMB_URL ) . 'js/field.select.js', array( 'jquery', 'select2', 'cmb-scripts' ) );
 	}
 
@@ -1071,7 +1071,7 @@ class CMB_Select extends CMB_Field {
 
 		parent::enqueue_styles();
 
-		wp_enqueue_style( 'select2', trailingslashit( CMB_URL ) . 'js/vendor/select2/select2.css' );
+		wp_enqueue_style( 'select2', trailingslashit( CMB_URL ) . 'js/vendor/select2/select2.min.css', array(), '4.0.3' );
 	}
 
 	public function html() {
