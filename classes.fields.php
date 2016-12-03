15efb99f4273b3ee4570469eecf64c723e4851a0
mikeselander <mikeselander@gmail.com>
2016-12-03 21:44:46 +0100

    Moved new field type into its own file

--- classes.fields.php
+++ classes.fields.php
@@ -811,44 +811,6 @@ class CMB_Checkbox extends CMB_Field {
 
 }
 
-
-class CMB_Checkbox_Multi extends CMB_Field {
-
-	public function html() {
-
-		if ( $this->has_data_delegate() ) {
-			$this->args['options'] = $this->get_delegate_data();
-		}
-
-		foreach ( $this->args['options'] as $i => $label ) :
-
-			$value = $this->get_value();
-
-			?>
-
-			<div class="cmb-checkbox-wrap">
-
-				<input
-					type="checkbox"
-					<?php $this->id_attr( 'item-' . $i ); ?>
-					<?php $this->boolean_attr(); ?>
-					<?php $this->class_attr(); ?>
-					<?php $this->name_attr( '[item-' . $i . ']' ); ?>
-					<?php checked( isset( $value[ 'item-' . $i ] ) ); ?>
-				/>
-
-				<label <?php $this->for_attr( 'item-' . $i ); ?>>
-					<?php echo esc_html( $label ); ?>
-				</label>
-
-			</div>
-
-		<?php endforeach;
-
-	}
-
-}
-
 /**
  * Standard title used as a splitter.
  *
