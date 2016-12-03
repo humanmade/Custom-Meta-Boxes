c4576c24f11c890f456a6a6a4f2265b608094c66
mikeselander <mikeselander@gmail.com>
2016-12-03 18:02:22 +0100

    Added tests and wrapper output for hidden field

--- fields/class-cmb-hidden-field.php
+++ fields/class-cmb-hidden-field.php
@@ -16,6 +16,24 @@
 class CMB_Hidden_Field extends CMB_Field {
 
 	/**
+	 * Print out a field wrapper.
+	 *
+	 * Overriding this as there's no reason nor opportunity to have a repeatable hidden field and we don't
+	 * want to display the title and description here so getting rid of all of that logic.
+	 */
+	public function display() {
+		?>
+
+		<div class="field-item" data-class="<?php echo esc_attr( get_class( $this ) ); ?>" style="display: none; <?php echo esc_attr( $this->args['style'] ); ?>">
+
+			<?php $this->html(); ?>
+
+		</div>
+
+		<?php
+	}
+
+	/**
 	 * Print out field HTML.
 	 */
 	public function html() {
