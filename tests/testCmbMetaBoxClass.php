a0b3710b281fc33c0aa2b89eb39f17936b2a1a28
mikeselander <mikeselander@gmail.com>
2016-12-22 09:42:08 +0100

    Cleaned up the base class tests a bit. Still not in love, but better

--- tests/testCmbMetaBoxClass.php
+++ tests/testCmbMetaBoxClass.php
@@ -10,6 +10,20 @@ class CmbMetaboxClassTests extends WP_UnitTestCase {
 	private $post_id = 0;
 
 	/**
+	 * Holds a simlpe fieldset.
+	 *
+	 * @var array
+	 */
+	private $simple_fields = [];
+
+	/**
+	 * Holds a simlpe fieldset.
+	 *
+	 * @var array
+	 */
+	private $group_fields = [];
+
+	/**
 	 * Initialization.
 	 */
 	function setUp() {
@@ -22,6 +36,43 @@ class CmbMetaboxClassTests extends WP_UnitTestCase {
 				'post_title'   => 'My test post',
 				'post_type'    => 'post',
 				'post_status'  => 'publish',
+			)
+		);
+
+		$this->simple_fields = array(
+			'title' => 'Simple',
+			'pages' => 'post',
+			'fields' => array(
+				array(
+					'id'         => 'text',
+					'name'       => 'Simple',
+					'type'       => 'text',
+					'repeatable' => false,
+				),
+			)
+		);
+
+		$this->group_fields = array(
+			'title' => 'Simple',
+			'pages' => 'post',
+			'fields' => array(
+				array(
+					'id'         => 'text',
+					'name'       => 'Simple',
+					'type'       => 'group',
+					'fields'     => array(
+						array(
+							'id'         => 'text',
+							'name'       => 'Simple',
+							'type'       => 'text',
+						),
+						array(
+							'id'         => 'text2',
+							'name'       => 'Simple 2',
+							'type'       => 'text',
+						),
+					),
+				),
 			)
 		);
 
@@ -38,154 +89,97 @@ class CmbMetaboxClassTests extends WP_UnitTestCase {
 
 	function test_values_from_init_non_repeatable() {
 
-		$simple_fields = array(
-			'title' => 'Simple',
-			'pages' => 'post',
-			'fields' => array(
-				array(
-					'id'         => 'text',
-					'name'       => 'Simple',
-					'type'       => 'text',
-					'repeatable' => false,
-				),
-			)
-		);
+		$values = array( 1, 2, 3, 4, 5 );
 
-		$boxes = new CMB_Meta_Box( $simple_fields );
-		$boxes->init_fields( $this->post_id );
-
-		// Save some values for testing.
-		$boxes->fields[0]->save( $this->post_id, array( 1, 2, 3, 4, 5 ) );
-
-		// Re-initialize the boxes and check the data.
-		$boxes->init_fields( $this->post_id );
+		$field = $this->intialize_fields( $this->simple_fields, $values );
 
 		// Check that a non-repeatable field only returns one value.
 		// The new instance of the field is 1.
-		$this->assertEquals( array( 1 ), $boxes->fields[1]->values );
+		$this->assertEquals( array( 1 ), $field->values );
+
+		unset( $boxes );
 	}
 
 	function test_values_from_init_repeatable() {
 
-		$simple_fields = array(
-			'title' => 'Simple',
-			'pages' => 'post',
-			'fields' => array(
-				array(
-					'id'         => 'text',
-					'name'       => 'Simple',
-					'type'       => 'text',
-					'repeatable' => true,
-				),
-			)
-		);
+		// Change simple field to be a repeatable.
+		$this->simple_fields['fields'][0]['repeatable'] = true;
 
-		$boxes = new CMB_Meta_Box( $simple_fields );
-		$boxes->init_fields( $this->post_id );
+		$values = array( 1, 2, 3, 4, 5 );
 
-		// Save some values for testing.
-		$boxes->fields[0]->save( $this->post_id, array( 1, 2, 3, 4, 5 ) );
-
-		// Re-initialize the boxes and check the data.
-		$boxes->init_fields( $this->post_id );
+		$field = $this->intialize_fields( $this->simple_fields, $values );
 
 		// Check that a non-repeatable field only returns one value.
 		// The new instance of the field is 1.
-		$this->assertEquals( array( 1, 2, 3, 4, 5 ), $boxes->fields[1]->values );
+		$this->assertEquals( array( 1, 2, 3, 4, 5 ), $field->values );
+
+		unset( $boxes );
 
 	}
 
 	function test_values_from_init_group_non_repeatable() {
 
-		$group_fields = array(
-			'title' => 'Simple',
-			'pages' => 'post',
-			'fields' => array(
-				array(
-					'id'         => 'text',
-					'name'       => 'Simple',
-					'type'       => 'group',
-					'fields'     => array(
-						array(
-							'id'         => 'text',
-							'name'       => 'Simple',
-							'type'       => 'text',
-						),
-						array(
-							'id'         => 'text2',
-							'name'       => 'Simple 2',
-							'type'       => 'text',
-						),
-					),
-				),
-			)
-		);
-
-		$boxes = new CMB_Meta_Box( $group_fields );
-		$boxes->init_fields( $this->post_id );
-
-		// Save some values for testing.
-		$boxes->fields[0]->save( $this->post_id, array(
-			array(
-				'text'  => array( 'one', 'two', 'three' ),
-				'text2' => array( 'four', 'five', 'six' ),
-			)
+		$values = array( array(
+			'text'  => array( 'one', 'two', 'three' ),
+			'text2' => array( 'four', 'five', 'six' ),
 		) );
 
-		// Re-initialize the boxes and check the data.
-		$boxes->init_fields( $this->post_id );
+		$field = $this->intialize_fields( $this->group_fields, $values );
 
 		// Check that a non-repeatable field only returns one value.
 		// The new instance of the field is 1.
-		$this->assertEquals( array( array( 'text' => 'one', 'text2' => 'four' ) ), $boxes->fields[1]->values );
+		$this->assertEquals( array( array( 'text' => 'one', 'text2' => 'four' ) ), $field->values );
+
+		unset( $boxes );
 
 	}
 
 	function test_values_from_init_group_repeatable() {
 
-		$group_fields = array(
-			'title' => 'Simple',
-			'pages' => 'post',
-			'fields' => array(
+		// Change fields to be a repeatable.
+		$this->group_fields['fields'][0]['fields'][0]['repeatable'] = true;
+		$this->group_fields['fields'][0]['fields'][1]['repeatable'] = true;
+
+		$values = array( array(
+			'text'  => array( 'one', 'two', 'three' ),
+			'text2' => array( 'four', 'five', 'six' ),
+		) );
+
+		$field = $this->intialize_fields( $this->group_fields, $values );
+
+		// Check that a non-repeatable field only returns one value.
+		// The new instance of the field is 1.
+		$this->assertEquals(
+			array(
 				array(
-					'id'         => 'text',
-					'name'       => 'Simple',
-					'type'       => 'group',
-					'fields'     => array(
-						array(
-							'id'         => 'text',
-							'name'       => 'Simple',
-							'type'       => 'text',
-							'repeatable' => true,
-						),
-						array(
-							'id'         => 'text2',
-							'name'       => 'Simple 2',
-							'type'       => 'text',
-							'repeatable' => true,
-						),
-					),
-				),
-			)
+					'text' => array( 'one', 'two', 'three' ),
+					'text2' => array( 'four', 'five', 'six' ),
+				)
+			),
+			$field->values
 		);
 
-		$boxes = new CMB_Meta_Box( $group_fields );
+		unset( $boxes );
+
+	}
+
+	/**
+	 * Mock up a CMB_MetaBox instance, save a field value and the return the boxes for testing
+	 *
+	 * @param $fields
+	 * @param $values
+	 * @return object Field type class instance.
+	 */
+	private function intialize_fields( $fields, $values ) {
+		$boxes = new CMB_Meta_Box( $fields );
 		$boxes->init_fields( $this->post_id );
 
 		// Save some values for testing.
-		$boxes->fields[0]->save( $this->post_id, array(
-			array(
-				'text'  => array( 'one', 'two', 'three' ),
-				'text2' => array( 'four', 'five', 'six' ),
-			)
-		) );
+		$boxes->fields[0]->save( $this->post_id, $values );
 
 		// Re-initialize the boxes and check the data.
 		$boxes->init_fields( $this->post_id );
 
-		// Check that a non-repeatable field only returns one value.
-		// The new instance of the field is 1.
-		$this->assertEquals( array( array( 'text' => array( 'one', 'two', 'three' ), 'text2' => array( 'four', 'five', 'six' ) ) ), $boxes->fields[1]->values );
-
+		return $boxes->fields[1];
 	}
 }
